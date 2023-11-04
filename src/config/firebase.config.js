f083c8b2345c84c1191ab9fc303084e58d2d6ccc
tyleramirault <tyler@hottakesapp.com>
2023-11-04 17:10:04 -0400

    firebase config change

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,10 +1,24 @@
+//DO NOT CONNECT TOT HIS UNDER ANY CIRCUMSTANCES
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyC6-N0k7ZzaJKpqlOFYXzi4NQ1LpMT_32g",
+//   authDomain: "hot-takes-65ae9.firebaseapp.com",
+//   databaseURL: "https://hot-takes-65ae9.firebaseio.com",
+//   projectId: "hot-takes-65ae9",
+// ------------------------------------------
+//   storageBucket: "COLD-takes-65ae9.appspot.com",
+// ------------------------------------------
+//   messagingSenderId: "363808171345",
+//   appId: "1:363808171345:web:5ab4ee15b61b5319531f70",
+//   measurementId: "G-9704FM0P88",
+// };
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyC6-N0k7ZzaJKpqlOFYXzi4NQ1LpMT_32g",
-  authDomain: "hot-takes-65ae9.firebaseapp.com",
-  databaseURL: "https://hot-takes-65ae9.firebaseio.com",
-  projectId: "hot-takes-65ae9",
-  storageBucket: "hot-takes-65ae9.appspot.com",
-  messagingSenderId: "363808171345",
-  appId: "1:363808171345:web:5ab4ee15b61b5319531f70",
-  measurementId: "G-9704FM0P88",
+  apiKey: "AIzaSyAXMln9MWQoUNmW020aq1pDob4q3uPUJG8",
+  authDomain: "hottakes-v2.firebaseapp.com",
+  projectId: "hottakes-v2",
+  storageBucket: "hottakes-v2.appspot.com",
+  messagingSenderId: "592898031284",
+  appId: "1:592898031284:web:89a60c34bc5f63fc58a1e1",
+  measurementId: "G-8L728P5DQL",
 };
