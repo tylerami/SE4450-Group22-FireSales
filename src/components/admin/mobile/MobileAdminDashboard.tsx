eee4cfe420e5b8f6f47b37b47e3306ab31603ca7
KevinJing <kjing2@uwo.ca>
2023-12-12 13:49:54 -0500

    added back button to admin dashboard

--- src/components/admin/mobile/MobileAdminDashboard.tsx
+++ src/components/admin/mobile/MobileAdminDashboard.tsx
@@ -1,19 +1,34 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import { useGlobalState } from "components/utils/GlobalState";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const MobileAdminDashboard = (props: Props) => {
+  const navigate = useNavigate();
+
+  const { setActiveTabIndex } = useGlobalState();
+
   return (
     <Flex
       w="100%"
       h="100%"
       p={6}
+      gap={8}
       direction={"column"}
       justifyContent={"center"}
       alignItems={"center"}
     >
       <Heading>Admin access is not available on mobile.</Heading>
+      <Button
+        onClick={() => {
+          setActiveTabIndex(0);
+          navigate("/");
+        }}
+      >
+        Back
+      </Button>
     </Flex>
   );
 };
